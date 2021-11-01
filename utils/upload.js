17ab30cc7dc609277ec9e78099aa81d52a752e6b
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2021-11-01 17:44:14 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
 
 const storage = new GridFsStorage({
-    url: 'mongodb+srv://user:codeforinterview27@cluster0.dmwwy.mongodb.net/BLOG?retryWrites=true&w=majority',
+    url:'',
     // url: 'mongodb://localhost:27017/image-upload',
     options: { useUnifiedTopology: true, useNewUrlParser: true },
     file: (request, file) => {
